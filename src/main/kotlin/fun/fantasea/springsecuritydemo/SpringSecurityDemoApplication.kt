1aa6a1a658fb839967124df9bad8fd8341fec511
Ant00000ny <Ant00000ny@icloud.com>
2024-12-24 11:24:27 +0800

    init

---
+++ src/main/kotlin/fun/fantasea/springsecuritydemo/SpringSecurityDemoApplication.kt
@@ -0,0 +1,11 @@
+package `fun`.fantasea.springsecuritydemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSecurityDemoApplication>(*args)
+}
---
+++ src/test/kotlin/fun/fantasea/springsecuritydemo/SpringSecurityDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package `fun`.fantasea.springsecuritydemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
